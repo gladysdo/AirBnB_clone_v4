5845666e8eb2886ee4cc5820d82d5cfa5038640f
gladysdo <doe.glad@gmail.com>
2024-01-15 00:03:16 +0000

    update

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -8,7 +8,7 @@ from models.place import Place
 from os import environ
 from flask import Flask, render_template
 import uuid
-app = Flask(__name__)
+app = Flask('web_dynamic')
 # app.jinja_env.trim_blocks = True
 # app.jinja_env.lstrip_blocks = True
 
@@ -22,8 +22,6 @@ def close_db(error):
 @app.route('/0-hbnb', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
-    cache_id = str(uuid.uuid4())
-    return render_template('0-hbnb.html', cache_id=cache_id)
     states = storage.all(State).values()
     states = sorted(states, key=lambda k: k.name)
     st_ct = []
@@ -36,11 +34,12 @@ def hbnb():
 
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
-
+    cache_id = uuid.uuid4()
     return render_template('0-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
-                           places=places)
+                           places=places,
+			   cache_id=cache_id)
 
 
 if __name__ == "__main__":
